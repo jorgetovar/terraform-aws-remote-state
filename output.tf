e1ee1bf42463c4d8e6e5bfd059bbb56a36b23559
jorgetovar <jorgetovar621@gmail.com>
2023-07-24 20:37:44 -0500

    remote state in aws with s3 and dynamodb

--- output.tf
+++ output.tf
@@ -1,5 +1,6 @@
 output "config" {
-  value = {
+  description = "A map containing the following configuration details: The name of the created S3 bucket, the AWS region of the S3 bucket, the ARN of the IAM role created for the backend, and the name of the created DynamoDB table for locking."
+  value       = {
     bucket         = aws_s3_bucket.state_bucket.bucket
     region         = data.aws_region.current.name
     role_arn       = aws_iam_role.iam_role.arn
